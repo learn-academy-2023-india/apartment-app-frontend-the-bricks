d85ab588e69a4719cc708c8265d8f356a88f5252
tetrevino <tomas_trevino@yahoo.com>
2024-02-22 11:03:53 -0800

    resolving conflicts

--- src/pages/ApartmentIndex.js
+++ src/pages/ApartmentIndex.js
@@ -56,29 +56,3 @@ const ApartmentIndex = ({ apartments }) => {
 }
 
 export default ApartmentIndex
-// const CatIndex = ({ cats }) => {
-//   return (
-//     <>
-//       <div className="meet-and-greet">
-//         <h3>Meet Your Fellow Felines</h3>
-//       </div>
-//       <div className="cat-index-section">
-//         {cats?.map((cat) => {
-//           return (
-//             <div key={cat.id} className="cat-profile-container">
-//               <img
-//                 src={cat.image}
-//                 alt="profile of all our cats friends"
-//                 className="cat-profile-pic"
-//               />
-//               <RouterNavLink to={`/catshow/${cat.id}`} className="cat-name-link">
-//                 <p>{cat.name}</p>
-//               </RouterNavLink>
-//             </div>
-//           )
-//         })}
-//       </div>
-//     </>
-//   )
-// }
-// export default CatIndex
