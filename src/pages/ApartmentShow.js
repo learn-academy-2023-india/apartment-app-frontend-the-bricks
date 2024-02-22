f50b9d438994390f0bf5e4d650d76db192e4e96e
Gr8Haus <garry.greathouse@gmail.com>
2024-02-21 16:58:21 -0800

    end of day team push

--- src/pages/ApartmentShow.js
+++ src/pages/ApartmentShow.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react"
+import { useParams, NavLink } from "react-router-dom"
+import { Card, CardImg, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-const ApartmentShow = () => {
-  return <div>ApartmentShow</div>
+const ApartmentShow = ({ apartments}) => {
+  const { id } = useParams()
+  const currentApartment = apartments.find((apartment) => apartment.id === +id)
+
+  if (!apartments) {
+    return <>Apartment not found!</>
+  }
+  
+  return (
+    <>
+    <div className="apart-body-index">
+      {currentApartment &&      
+        <Card className='flex-apart-index'>
+          {/* Where picture goes */}
+          <CardImg top width="100%" src={currentApartment?.image} alt="" />
+          {/* Changes text font */}
+          <CardBody className="apart-text-index">
+            {/* Shows apartment info */}
+            <div className="grid-row">
+              <div className="apart-text-index">
+                <CardTitle><b>{currentApartment?.price} Per Month</b></CardTitle>
+                <CardSubtitle>{currentApartment?.street}, {currentApartment?.city}, {currentApartment?.state}</CardSubtitle>
+                <CardSubtitle>{currentApartment.bedrooms} Bedroom,  {currentApartment?.bathrooms} Bath</CardSubtitle>
+                <CardSubtitle>Pets: {currentApartment?.pets}</CardSubtitle>
+                <CardSubtitle>Details: {currentApartment?.details}</CardSubtitle>
+              </div>
+            </div>
+            {/* Button takes back to listings */}
+            <NavLink to={`/apartmentindex`} className="nav-link">
+              <Button className="apart-button-index">Back to Listings</Button>
+            </NavLink>
+          </CardBody>
+        </Card>
+      }
+    </div>
+    </>
+  )
 }
 
-export default ApartmentShow
+export default ApartmentShow;
